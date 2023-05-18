dc7065cef224cf47cc10b90c4258b064770ddf1a
Manonthemon <klimowicz.maciej@gmail.com>
2023-05-18 11:29:10 +0100

    Added funciton to update html with a list to display items on page

--- script.js
+++ script.js
@@ -11,7 +11,18 @@ function addItem(e) {
         done: false
     }
 items.push(item)
+populateList(items, itemsList)
 this.reset();
+}
+
+function populateList(plates = [], platesList) {
+platesList.innerHTML = plates.map((plate, i) => {
+return `
+<li> 
+<label for="">${plate.text}</label>
+</li>
+`
+}).join('')
 }
 
 addItems.addEventListener('submit', addItem)
